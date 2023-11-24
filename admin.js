ddea7ec7f1648c6271f50eef82b7975deada219e
Murat <muratkairbek908@gmail.com>
2023-11-25 02:08:50 +0600

    admin panel changing

--- admin.js
+++ admin.js
@@ -1,11 +1,18 @@
-var userList = document.getElementById("userList");
+var userTable = document.getElementById("userList");
 var users = JSON.parse(sessionStorage.getItem("users"));
+var output = false
 
 function outputUsers(){
-	for (let i = 0; i < users.length; i++) {
-		userList.innerHTML += `<li id="user${i}" class="container p-2 m-2 border border-danger rounded"> login: ${users[i].login}; email: ${users[i].email}; passwd: ${users[i].passwd}; <button onclick="deleteItem(${i})">delete</button> <button onclick="edit(${i})">edit</button></li>` 
+	if (output === false) {
+		userTable.style.display="block";
+		for (let i = 1; i <= users.length; i++) {
+			userTable.innerHTML += `<tr id="user${i}"><td>${i}.</td><td id="user${i}-login">${users[i].login}</td> <td id="user${i}-email">${users[i].email}</td> <td id="user${i}-passwd">${users[i].passwd}</td></tr>`
+		}
+		userList.innerHTML += `<button class="p-3 m-3 btn btn-primary" id="menucloser" onclick="deleteMenu()">Remove menu</button>`
+		output = true
+	} else {
+		alert("You already has output")
 	}
-	userList.innerHTML += `<br> <button onclick="hide()">Hide menu<button>`
 }
 
 function edit(num) {
@@ -59,6 +66,14 @@ function deleteItem(num){
 	deleted.remove();
 }
 
-function hide(){
-	userList.style.display = "none";	
+function deleteMenu(){
+	userTable.style.display="none";
+	output = false
+	for (let i = 0; i < users.length; i++) {
+		let elem = document.getElementById(`user${i}`)
+		elem.remove()
+		//userTable.innerHTML += `<tr id="user${i}"><td id="user${i}-login">${users[i].login}</td> <td id="user${i}-email">${users[i].email}</td> <td id="user${i}-passwd">${users[i].passwd}</td></tr>`
+	}
+	let closer = document.getElementById("menucloser")
+	closer.remove()
 }
